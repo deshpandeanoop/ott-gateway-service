b1876cbaef0ed256661f3b2ccc6033203b007afb
deshpandeanoop <anoop.bhardwaja@gmail.com>
2021-10-23 16:28:32 +0530

    Initial commit(maven project structure)

---
+++ src/main/java/com/github/ott/gateway/service/ottgatewayservice/OttGatewayServiceApplication.java
@@ -0,0 +1,13 @@
+package com.github.ott.gateway.service.ottgatewayservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OttGatewayServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OttGatewayServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/ott/gateway/service/ottgatewayservice/OttGatewayServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.ott.gateway.service.ottgatewayservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OttGatewayServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
