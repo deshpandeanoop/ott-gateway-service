a41c165c67fc419f74f3bccdb760836d34b5c831
deshpandeanoop <anoop.bhardwaja@gmail.com>
2021-11-28 10:22:21 +0530

    Added filters to OTT details

--- src/main/java/com/github/ott/gateway/service/controller/OttAccountServiceController.java
+++ src/main/java/com/github/ott/gateway/service/controller/OttAccountServiceController.java
@@ -1,12 +1,19 @@
 package com.github.ott.gateway.service.controller;
 
+import com.github.ott.gateway.service.data.db.OttServiceAccount;
 import com.github.ott.gateway.service.data.request.AddOttServiceAccRequest;
 import com.github.ott.gateway.service.data.response.GetOttServiceAccResponse;
+import com.github.ott.gateway.service.enums.OttPlatformType;
 import com.github.ott.gateway.service.repository.OttServiceAccRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequestMapping("/v1/ottaccounts")
 @RestController
@@ -23,9 +30,32 @@ public class OttAccountServiceController {
     }
 
     @GetMapping
-    public ResponseEntity<GetOttServiceAccResponse> getOttAccounts(@RequestParam("gatewayUserId") String gatewayUserId) {
+    public ResponseEntity<GetOttServiceAccResponse> getOttAccounts(@RequestParam("gatewayUserId") String gatewayUserId,
+                                                                   @RequestParam("fetchOnlyNames") boolean fetchOnlyNames,
+                                                                   @RequestParam("platformType") String platformType) {
         GetOttServiceAccResponse getOttServiceAccResponse = new GetOttServiceAccResponse();
-        getOttServiceAccResponse.setOttServiceAccounts(ottServiceAccRepository.getByGateWayServiceUserId(gatewayUserId));
+        List<OttServiceAccount> serviceAccounts = ottServiceAccRepository.getByGateWayServiceUserId(gatewayUserId);
+        /**
+         * Ideally the predicate push down should happen, since the product is in prototyping phase
+         * adding in the filtering logic in the controller layer for now.
+         */
+
+        if(StringUtils.hasText(platformType) && OttPlatformType.isValid(platformType)) {
+            serviceAccounts = Collections.singletonList(serviceAccounts
+                    .stream()
+                    .filter(ottServiceAccount -> ottServiceAccount.getPlatformType().name().equals(platformType))
+                    .findFirst().get());
+        }
+
+        if(fetchOnlyNames) {
+            log.info("FetchOnlyNames flag is passed as true, extracting and returning only OTT platform names");
+            serviceAccounts = serviceAccounts
+                    .stream()
+                    .map(serviceAcc -> new OttServiceAccount("", "", serviceAcc.getPlatformType(), serviceAcc.getGateWayServiceUserId()))
+                    .collect(Collectors.toList());
+        }
+        getOttServiceAccResponse.setOttServiceAccounts(serviceAccounts);
+
         return ResponseEntity.ok(getOttServiceAccResponse);
     }
 }
--- src/main/java/com/github/ott/gateway/service/enums/OttPlatformType.java
+++ src/main/java/com/github/ott/gateway/service/enums/OttPlatformType.java
@@ -1,8 +1,15 @@
 package com.github.ott.gateway.service.enums;
 
+import java.util.Arrays;
+
 /**
  * Holds type of OTT platform supported by the gateway platform
  */
 public enum OttPlatformType {
     AMAZON_PRIME, NETFLIX;
+
+    public static boolean isValid(String val) {
+        return Arrays.stream(values())
+                .anyMatch(el -> el.name().equals(val));
+    }
 }
